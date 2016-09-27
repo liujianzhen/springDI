82887f3ce7af7712ddc062d594bf9e98fcd562ab
liujianzhen <1550040296@qq.com>
2016-09-27 16:15:06 +0800

    添加注释

--- src/springDI/DamselRescuingKnight.java
+++ src/springDI/DamselRescuingKnight.java
@@ -6,6 +6,7 @@ public class DamselRescuingKnight {
 
 	private Quest quest;
 	
+	//不创建Quest的实例，构造注入
 	public DamselRescuingKnight(Quest quest) {
 		this.quest = quest;
 	}
@@ -14,7 +15,9 @@ public class DamselRescuingKnight {
 		quest.embark();
 	}
 	public static void main(String[] args) {
+		//获取IOC容器，相当于一个工厂
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//直接从容器获取DamselRescuingKnight实例，而不必知道其创建过程，若想替换传入的Quest参数，在配置文件中修改即可
 		DamselRescuingKnight knight = context.getBean(DamselRescuingKnight.class);
 		knight.execute();
 	}
